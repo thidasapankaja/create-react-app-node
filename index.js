9127482653c009cd82b03749f137154014a51b66
pankaja92 <pankajanc@gmail.com>
2018-03-24 19:48:38 +0530

    Serve content files from server

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/api/reply', (req, res) =>{
-    res.send({ reply : 'This message is from Server !'});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.get("/api/reply", (req, res) => {
+  res.send({ reply: "This message is from Server !" });
 });
 
 app.listen(port);
