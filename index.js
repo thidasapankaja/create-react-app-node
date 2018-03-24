86b340077e712643694d8c0c020594a30740f6aa
pankaja92 <pankajanc@gmail.com>
2018-03-24 20:02:20 +0530

    Fix API requests

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//Serve static files
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
+//Serve API request from client
 app.get("/api/reply", (req, res) => {
+    console.log('Requested from Client');
   res.send({ reply: "This message is from Server !" });
+});
+
+//Serve other requests from react app
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(port);
